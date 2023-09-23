be0c44a859b41124c8d72d80a8c566608203e479
Alex <alex_sizow@outlook.com>
2023-09-23 23:46:03 +0300

    Longest Substring Without Repeating Characters solution

---
+++ solutions/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,40 @@
+// Given a string s, find the length of the longest
+// substring
+//  without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+function lengthOfLongestSubstring(s) {
+  if (s.length <= 1) {
+    return s.length;
+  }
+
+  const set = new Set();
+  let left = 0;
+  let maxSize = 0;
+
+  for (let i in s) {
+    while (set.has(s[i])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[i]);
+    maxSize = Math.max(maxSize, i - left + 1);
+  }
+
+  return maxSize;
+}
