91b005e95715f3573cd286ac79d4a7345e7b9319
Alex <alex_sizow@outlook.com>
2023-11-06 23:22:45 +0300

    Count of Distinct Integers After Reverse

--- counter.js
+++ counter.js
@@ -1,9 +1,25 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+async function sendPostRequest() {
+  try {
+    const response = await fetch(
+      "https://dev-lawyer.dev.tdsgn.ru/api/auth/recovery",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "mail@tdsgn.ru",
+        }),
+      }
+    );
+    if (response.ok) {
+      console.log("POST request successful");
+    } else {
+      console.log("POST request failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
 }
+
+sendPostRequest();
